6fcb846c9366c50b63aa472d628f9932a477a0b3
Daqinpro <3460474099@qq.com>
2024-10-24 15:01:55 +0800

    Initial commit

---
+++ python2.py
@@ -0,0 +1,23 @@
+# a = 88
+# if a>=0:
+# 	print("Yes")
+# else:
+# 	print("No")
+
+
+# print(int(88.88))
+
+str1 = "两'点'水"
+str2 = "两\"点\"水"
+
+print(str1)
+print(str2)
+
+print(type(str1))
+
+#计算：就是**表示幂
+#//整除一次返回商的整数部分
+
+#对于布尔值，可以使用and or not运算
+
+#空值 可以用None表示
